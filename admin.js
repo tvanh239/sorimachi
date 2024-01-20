1b5647a2b0edc82ed43e3cc132cb12e0d00be785
Vietanh239 <72076528+Vietanh239@users.noreply.github.com>
2024-01-20 22:27:58 +0700

    commit ckeditor

--- admin.js
+++ admin.js
@@ -1,6 +1,30 @@
-function A(){
-    alert("123");
-}
 
-var show-kikan = document.getElementById("show-kikan");
+function openDialog(mode){
+    $("#dialog").dialog({
+        modal:true,
+        autoOpen: false,
+        draggable: true,
+        title: '回答追加',
+        width: 500,
+        height: 500
+    });
+    ClassicEditor
+		.create( document.querySelector( '#editor' ), {
+			// toolbar: [ 'heading', '|', 'bold', 'italic', 'link' ]
+		} )
+		.then( editor => {
+			window.editor = editor;
+		} )
+		.catch( err => {
+			console.error( err.stack );
+		} );
+    $(".ui-dialog-titlebar-close").hide();
+    $(".ui-dialog-titlebar").css("background","#fff");
+    $(".ui-dialog-titlebar").css("border","none");
+    $(".ui-dialog-titlebar").css("padding","0");
+    $(".ui-dialog-titlebar").css("padding-left","15px");
+    $(".ui-dialog-titlebar").css("font-size","18px");
+    $("#dialog").dialog("open");
+    
+}
 
