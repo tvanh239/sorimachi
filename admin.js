8d138db8d9ee7b04d4cf40f54153a256dfa1fd34
Vietanh239 <72076528+Vietanh239@users.noreply.github.com>
2024-01-21 12:03:38 +0700

    change dialog

--- admin.js
+++ admin.js
@@ -5,12 +5,25 @@ function openDialog(mode){
         autoOpen: false,
         draggable: true,
         title: '回答追加',
-        width: 500,
-        height: 500
+        width: 800,
+        height: 800
     });
     ClassicEditor
 		.create( document.querySelector( '#editor' ), {
 			// toolbar: [ 'heading', '|', 'bold', 'italic', 'link' ]
+            
+		} )
+		.then( editor => {
+			window.editor = editor;
+		} )
+		.catch( err => {
+			console.error( err.stack );
+		} );
+    
+    ClassicEditor
+		.create( document.querySelector( '#editor2' ), {
+			// toolbar: [ 'heading', '|', 'bold', 'italic', 'link' ]
+            
 		} )
 		.then( editor => {
 			window.editor = editor;
@@ -28,3 +41,53 @@ function openDialog(mode){
     
 }
 
+document.addEventListener("DOMContentLoaded", () => {
+  const dropZone = document.getElementById("dropZone");
+
+  // Create hidden file input
+  const fileInput = document.createElement("input");
+  fileInput.type = "file";
+  fileInput.multiple = true;
+  fileInput.style.display = "none";
+  fileInput.addEventListener("change", () => handleFiles(fileInput.files));
+  
+  // Append file input to main container
+  dropZone.appendChild(fileInput);
+
+  dropZone.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    dropZone.classList.add("dragged-over");
+  });
+
+  dropZone.addEventListener("dragleave", () => {
+    dropZone.classList.remove("dragged-over");
+  });
+
+  dropZone.addEventListener("drop", (e) => {
+    e.preventDefault();
+    dropZone.classList.remove("dragged-over");
+    const files = e.dataTransfer.files;
+    handleFiles(files);
+  });
+
+  // Trigger file input on click
+  dropZone.addEventListener("click", () => {
+    fileInput.click();
+  });
+});
+
+function handleFiles(files) {
+  var string_file_name = "";
+  for(i=0;i<files.length;i++){
+    if(i==0){
+      string_file_name =  files[i].name;
+    }else{
+      string_file_name = string_file_name + ", " + files[i].name;
+    }
+    document.getElementById("dropText").innerText = string_file_name;
+  }
+}
+
+function closeDiaLog(){
+  $("#dialog").dialog("close");
+}
